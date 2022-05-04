424403e911c0dddaff64fca183ab12166e7e939c
andrebrum01 <41603045+andrebrum01@users.noreply.github.com>
2022-05-04 13:41:38 -0400

    Add files via upload

--- Class/Vector.js
+++ Class/Vector.js
@@ -48,12 +48,18 @@ class Vector {
         
     }
 
+    print(){
+        return `x: ${this.x}, y: ${this.y}`
+    }
     static dist(vector1,vector2){
         return Math.floor(Math.sqrt(Math.pow(vector1.x - vector2.x,2)+Math.pow(vector1.y - vector2.y,2)));
     }
 
     static add(vector1,vector2){
         return new Vector (vector1.x+vector2.x,vector1.y+vector2.y);
+    }
+    static sub(vector1,vector2){
+        return new Vector (vector1.x-vector2.x,vector1.y-vector2.y);
     }
     
 }
